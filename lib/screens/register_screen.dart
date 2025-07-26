dd771168a0d3438de61f9602d78e54933b53b9d3
Plinio Gonçalves <plinio.goncalves@verdecard.com.br>
2025-07-26 01:25:54 -0300

    feat: Implement user registration with Firebase

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,10 +27,102 @@ class RegisterScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Criar Conta'),
       ),
-      body: const Center(
-        child: Text(
-          'Aqui será a tela de Cadastro.',
-          style: TextStyle(fontSize: 24),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 40),
+
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+
+              SizedBox(height: 16),
+
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+              
+              SizedBox(height: 16),
+
+              TextField(
+                controller: _confirmPasswordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Confirmar Senha',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock_outline),
+                ),
+              ),
+
+              SizedBox(height: 32),
+
+              ElevatedButton(
+                onPressed: () async{
+                  final email = _emailController.text.trim();
+                  final password = _passwordController.text.trim();
+                  final confirmPassword = _confirmPasswordController.text.trim();
+
+                  if (email.isEmpty ||
+                      password.isEmpty ||
+                      confirmPassword.isEmpty) {
+                    print("Por favor, preencha todos os campos.");
+                    return;
+                  }
+
+                  if (password != confirmPassword) {
+                    print("As senhas não conferem.");
+                    return;
+                  }
+
+                  try {
+                    final userCredential = await FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+
+                    print(
+                      'Cadastro bem-sucedido! User ID: ${userCredential.user?.uid}',
+                    );
+
+                  } on FirebaseAuthException catch (e) {
+                    print('Ocorreu um erro no cadastro: ${e.code}');
+                    if (e.code == 'weak-password') {
+                      print('A senha fornecida é muito fraca.');
+                    } else if (e.code == 'email-already-in-use') {
+                      print(
+                        'O e-mail fornecido já está em uso por outra conta.',
+                      );
+                    } else {
+                      print('Erro desconhecido: ${e.message}');
+                    }
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                ),
+                child: Text(
+                  'Cadastrar',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
